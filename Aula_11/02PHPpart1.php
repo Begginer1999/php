28c260be27b33febe6587902203093d26329c356
Paulo Ramon <paulooramon@gmail.com>
2018-07-02 00:37:19 -0300

    coloquei um hello dude

--- Aula_11/02PHPpart1.php
+++ Aula_11/02PHPpart1.php
@@ -9,6 +9,7 @@
 <div>
 <form method="get" action="02PHPpart2.php">
     <?php
+        echo "hello dude";
         $c = 1;
         while($c<=5){
         echo "Valor $c: <input type='number' name='v$c' max='100' min='0' value='0'/> <br/>";
